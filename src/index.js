d73b95bb288f8fc34aa0514533cd9f4503237de1
Angelica Lopez <angelica.lopezcap@gmail.com>
2022-05-20 10:30:33 -0500

    Agregando inputs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import validator from './validator.js';
 
+
+
 console.log(validator);
+
+let btnValidar = document.getElementById("validar")
+btnValidar.addEventListener('click', () => {
+    let inputValue = document.getElementById("name").value
+    if(inputValue === ""){}  
+});
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,9 @@
 const validator = {
   // ...
+
 };
 
+
+
 export default validator;
+
