5b59bb9351bd321a83236e01ba94695edc3c1010
Angelica Lopez <angelica.lopezcap@gmail.com>
2022-05-27 00:25:35 -0500

    Cambios en los archivos: Validator.js y CSS

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import validator from './validator.js';
 
 const btnValidator = document.getElementById('btnValidator');
 const result = document.getElementById('result');
+const creditCardNumber = document.getElementById('creditCardNumber');
 
-btnValidator.addEventListener('click', () => {
-const creditCardNumber = document.getElementById('creditCardNumber').value;
-    
-    if(validator.isValid(creditCardNumber)  === true){
+
+btnValidator.addEventListener('click', (event) => {
+    event.preventDefault()
+    console.log("hola!", creditCardNumber.value);
+    if(validator.isValid(creditCardNumber.value)){
         result.innerHTML = "Tu tarjeta es válida";
     } else
         result.innerHTML = "Tu verificación falló, vuelve a intentarlo";
--- src/validator.js
+++ src/validator.js
@@ -2,26 +2,42 @@
 
 const validator = {
   isValid:(creditCardNumber) => {
+    
     const length = creditCardNumber.length;
-    let count = 0;
+    const reverse = creditCardNumber.split('').reverse();
+    //console.log(reverse)
+    let sum = 0;
     
   
-
+     
     for (let i = 0; i < length; i++) {
-      let currentDigit = parseInt(creditCardNumber[i]);
-      if ((i + 1) % 2 == 0) {
+      let currentDigit = parseInt(reverse[i]);
+      //let currentDigit = parseInt(creditCardNumber[i]);
+    //  console.log(currentDigit);
+      if (i  % 2 !== 0) {
+       // console.log()
         if ((currentDigit * 2) > 9){
-          currentDigit = currentDigit - 9;
+          currentDigit = currentDigit - 9;  
         }
-        else{
-          count = count + currentDigit;
-        }
-    
        
+        
       }
-      return (count % 10) === 0;
+      sum = sum + currentDigit;
+        //console.log(sum)
+        //return (sum % 10) === 0;
+
+       
+   // console.log(sum)
+      
     }
+   /*  if (sum % 10 === 0){
+      return true;
+      console.log("es valido")
+     }
+     return false; */
+     //console.log(validator.isValid('79927398713'))
   }
+
 };
 
 maskify:(creditCardNumber) => {
