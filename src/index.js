fcf19869e3e609d88354664eb98867e82e222ee0
Angelica Lopez <angelica.lopezcap@gmail.com>
2022-06-04 00:36:28 -0500

    AÃPAgregando página principal, secciones y cambios en el archivo style css

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import validator from './validator.js';
 
-
+const btnComprar = document.getElementById('btnComprar');
+const backButton = document.getElementById('backButton');
 const btnValidator = document.getElementById('btnValidator');
 const result = document.getElementById('result');
 const creditCardNumber = document.getElementById('creditCardNumber');
 
+btnComprar.addEventListener('click', () => {
+    const index = document.querySelector(".index");
+    const validation = document.querySelector(".validation");
+    index.style.display = "none";
+    validation.style.display = "block";
+});
+
 
 btnValidator.addEventListener('click', (event) => {
-   event.preventDefault()
+   event.preventDefault();
     //console.log("hola!", creditCardNumber.value);
     if(validator.isValid(creditCardNumber.value)){
         result.innerHTML = `Tu tarjeta es válida`;
     } else {
         result.innerHTML= `Tu verificación falló ${validator.maskify(creditCardNumber.value)}, ingresa un número válido`;
     }
+    });
+
+    backButton.addEventListener('click', (event) => {
+        const index = document.querySelector(".index");
+        const validation = document.querySelector(".validation");
+        index.style.display = "flex";
+        validation.style.display = "none";
     });
